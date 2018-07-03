81a6293dacb6da652938db58a174614d6ee74e05
ageng133 <39780039+ageng133@users.noreply.github.com>
2018-07-03 19:12:41 +0700

    ini

--- Admin/logout.php
+++ Admin/logout.php
@@ -1,9 +1,5 @@
 <?php
    session_start();
    session_destroy();
+   header('location:login.php');
 ?>
-
-<div align="center">
-  <h2>Anda telah berhasil logout..</h2>
-  Silahkan klik <a href="login.php">disini</a> untuk login kembali
-</div>
